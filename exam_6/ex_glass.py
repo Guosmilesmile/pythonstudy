cfd7a218f3c5d3b75535f2322abc02fd58f62965
Christy <511955993@qq.com>
2016-04-27 15:56:34 +0800

    ex6

--- exam_6/ex_glass.py
+++ exam_6/ex_glass.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import VotingClassifier
-
+from sklearn import cluster
 def getTargetData(fielname):
 	try:
 		file_object = open(fielname)
@@ -31,41 +31,15 @@ def getTargetData(fielname):
 
 train_text,train_lable = getTargetData("glass.data")
 test_text,test_lable = getTargetData("glass.test")
-clf1 = DecisionTreeClassifier(max_depth=4)
-clf2 = KNeighborsClassifier(n_neighbors=7)
-clf3 = SVC(kernel='rbf', probability=True)
-eclf = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2),('svc', clf3)],voting='soft', weights=[2, 1, 2])
-eclf2 = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2),('svc', clf3)],voting='hard')
 
-clf1.fit(train_text, train_lable)
-clf2.fit(train_text, train_lable)
-clf3.fit(train_text, train_lable)
-eclf.fit(train_text, train_lable)
-eclf2.fit(train_text, train_lable)
-result1 = clf1.predict(test_text)
-result2 = clf2.predict(test_text)
-result3 = clf3.predict(test_text)
-result4 = eclf.predict(test_text)
-result5 = eclf2.predict(test_text)
-error1 = 0
-error2 = 0
-error3 = 0
-error4 = 0
-error5 = 0
-for i in range(len(result1)):
-	if result1[i] != test_lable[i]:
-		error1 = error1 + 1
-	if result2[i] != test_lable[i]:
-		error2 = error2 + 1
-	if result3[i] != test_lable[i]:
-		error3 = error3 + 1
-	if result4[i] != test_lable[i]:
-		error4 = error4 + 1
-	if result5[i] != test_lable[i]:
-		error5 = error5 + 1
-error1 = (1- error1*0.1/len(result1))*100
-error2 = (1- error2*0.1/len(result1))*100
-error3 = (1- error3*0.1/len(result1))*100
-error4 = (1- error4*0.1/len(result1))*100
-error5 = (1- error5*0.1/len(result1))*100
-print error1,error2,error3,error4,error5
+
+#k_means = cluster.KMeans(n_clusters=7,precompute_distances =True).fit(train_text)
+
+
+#db = cluster.DBSCAN(min_samples =20,algorithm ='ball_tree').fit(train_text)
+
+#fa = cluster.FeatureAgglomeration().fit(train_text);
+ap = cluster.AffinityPropagation(convergence_iter = 50).fit(train_text);
+
+print ap.labels_
+#print train_lable
